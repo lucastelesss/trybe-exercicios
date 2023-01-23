a28af569d7007e0f9b2175ab6709c1f7674d20a5
lucastelesss <lucastelessouza005@gmail.com>
2023-01-23 15:18:04 -0300

    primeiro exercicio js

--- fundamentos/secao-3-introducao-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-3-introducao-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,5 +1,15 @@
-let age = 23;
-let nameGuy = "Hulk"
+const myName = "Lucas";
+const birthCity = "São Bento do Sul";
+let birthYear = 1999;
 
-console.log(age);
-console.log(nameGuy);
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030
+
+console.log(birthYear);
+
+birthCity = 2030;
+
+console.log(birthCity);
