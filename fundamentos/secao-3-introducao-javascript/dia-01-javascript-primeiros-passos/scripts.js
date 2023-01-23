99733fbdaccc5957660454849af87105d99ea2b7
lucastelesss <lucastelessouza005@gmail.com>
2023-01-23 18:28:27 -0300

    exercicios de javascript do dia 1

--- fundamentos/secao-3-introducao-javascript/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/secao-3-introducao-javascript/dia-01-javascript-primeiros-passos/scripts.js
@@ -1,15 +1,88 @@
-const myName = "Lucas";
-const birthCity = "São Bento do Sul";
-let birthYear = 1999;
+// const myName = "Lucas";
+// const birthCity = "São Bento do Sul";
+// let birthYear = 1999;
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
 
-birthYear = 2030
+// birthYear = 2030
 
-console.log(birthYear);
+// console.log(birthYear);
 
-birthCity = 2030;
+// birthCity = 2030;
 
-console.log(birthCity);
+// console.log(birthCity);
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientAge);
+
+// patientId = '50';
+
+// console.log(typeof patientId);
+
+// const base = 5;
+// const heigth = 8;
+// const area = base * heigth;
+// console.log(area)
+// const perimeter = base + base + heigth + heigth;
+// console.log(perimeter);
+
+// const notaCandidata = 59;
+
+// if(notaCandidata >= 80) {
+//     console.log("Parabéns. você faz pate do grupo das pessoas aprovadas")
+// } else if(notaCandidata < 80 && notaCandidata >= 60) {
+//     console.log("Você está na nossa lista de espera")
+// } else {
+//     console.log("Infelizmente você reprovou")
+// }
+
+// let comida = 'pão de queijo';
+// const bebida = 'cafézinho';
+// if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+//   console.log('Muito obrigado pela refeição :)');
+// } else {
+//   console.log('Acho que houve um engano com meu pedido');
+// }
+
+// const currentHour = 4;
+// let message;
+
+// if(currentHour >= 22) {
+//     message = 'Não deveríamos comer nada, é hora de dormir”'
+// } else if(currentHour >= 18 && currentHour < 22) {
+//     message = 'Rango da noite, vamos jantar :D'
+// } else if(currentHour >= 14 && currentHour < 18) {
+//     message = 'Vamos fazer um bolo pro café da tarde?'
+// }else if (currentHour >= 11 && currentHour < 14) {
+//     message = 'Hora do almoço!!!'
+// } else if (currentHour >= 4 && currentHour < 11) {
+//     message = 'Hmmm, cheiro de café recém-passado'
+// }
+
+// console.log(message);
+
+// let weekDay = 'Domingo'
+
+// if(weekDay === 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'quinta-feira' || 'sexta-feira') {
+// console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+// } else {
+//     console.log("FINALMENTE, descanso merecido UwU")
+// }
+
+let num1 = 7;
+let num2 = 5;
+
+if (num1 == num2) {
+    console.log(num1 + num2)
+} else {
+    console.log(num1 * num2)
+} 
---
+++ fundamentos/secao-3-introducao-javascript/dia-01-javascript-primeiros-passos/scripts2.js
@@ -0,0 +1,45 @@
+// let a = 5;
+// let b = 7;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+
+// const num1 = 3;
+// const num2 = 5;
+
+// if(num1 > num2) {
+//     console.log(num1)
+// } else {
+//     console.log(num2)
+// }
+
+// const num1 = 40;
+// const num2 = 11;
+// const num3 = 9;
+
+// if(num1 > num2 && num1 > num3) {
+//     console.log(num1)
+// } else if(num2 > num1 && num2 > num3) {
+//     console.log(num2)
+// } else if(num3 > num1 && num3 > num2) {
+//     console.log(num3)
+// }
+
+// let param = 0;
+
+// if(param > 0) {
+//     console.log("Positive")
+// } else if(param < 0) {
+//     console.log("Negative")
+// } else {
+//     console.log("zero")
+// }
+
+let ang1 = 180;
+let ang2 = 180;
+let ang3 = 180;
+
