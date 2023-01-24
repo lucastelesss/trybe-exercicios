aa892ff5a5fc67643f6969c319d3e27d88da90b7
lucastelesss <lucastelessouza005@gmail.com>
2023-01-24 16:02:56 -0300

    exercicios iniciais do dia

---
+++ fundamentos/secao-3-introducao-javascript/dia-02-javascript-array-loop-for/scripts.js
@@ -0,0 +1,82 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList[.length]);
+// // 3
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.pop('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// // ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push("Contato")
+// console.log(menu);
+
+// let cars = ["Saab", "Volvo", "BMW"];
+
+// for (let index = 0; index < cars.length; index += 1) {
+//   console.log(cars[index]);
+// }
+
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for(let index = 0; index < groceryList.length; index += 1) {
+//     console.log(groceryList[index])
+// }
+
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+
+// // resultado: 
+// //1
+// //2
+// //3
+// //4
+// //5
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let index of names) {
+//     console.log(index);
+// }
+
