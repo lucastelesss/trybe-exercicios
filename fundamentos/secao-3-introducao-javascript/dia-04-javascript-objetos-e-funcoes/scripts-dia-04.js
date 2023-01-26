11125bb167a44fddd3ec7f1a2f767c4d11c52e12
lucastelesss <lucastelessouza005@gmail.com>
2023-01-26 18:54:55 -0300

    terminando primeiros exercicios de funcoes

--- fundamentos/secao-3-introducao-javascript/dia-04-javascript-objetos-e-funcoes/scripts-dia-04.js
+++ fundamentos/secao-3-introducao-javascript/dia-04-javascript-objetos-e-funcoes/scripts-dia-04.js
@@ -144,3 +144,19 @@
 // }
 
 // console.log(numeroPositivoOuNegativo(0));
+
+
+
+//Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo.Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+
+// function retornaTriangulo(ang1, ang2, ang3) {
+//     if(ang1 + ang2 + ang3 === 180) {
+//         return true
+//     }else if(ang1 < 0 || ang2 < 0 || ang3 < 0) {
+//         return "Inválido"
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(retornaTriangulo(60, -50, 50));
