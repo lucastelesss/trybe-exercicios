4b2c1fa1e35b652710418927ddf8c71dbac279f1
lucastelesss <lucastelessouza005@gmail.com>
2023-01-26 18:20:14 -0300

    operaçoes aritmetica com funcoes

--- fundamentos/secao-3-introducao-javascript/dia-04-javascript-objetos-e-funcoes/scripts-dia-04.js
+++ fundamentos/secao-3-introducao-javascript/dia-04-javascript-objetos-e-funcoes/scripts-dia-04.js
@@ -1,1 +1,99 @@
 // objetos e funções!
+
+// let player = {
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+// }
+
+// player.bestInTheWOrld = [2006, 2007, 2008, 2009, 2010, 2018];
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars) {
+//   console.log(cars[index]);
+// }
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let index in car) {
+//     console.log(index, car[index]);
+//   }
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for(let index in names) {
+//     console.log('Olá ' + names[index])
+//   }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
+//   for(let index in car) {
+//     console.log(index, car[index])
+//   }
+
+
+
+// Adição
+
+function somaValores(a, b) {
+    return a + b
+}
+
+console.log(somaValores(10, 5))
+
+
+
+// //Subtração
+
+// function subtracao(numero1, numero2) {
+//     let resultado = numero1 - numero2;
+//     return resultado
+// }
+
+// console.log(subtracao(70, 20))
+
+
+
+// //Multiplicacao
+
+// function multiplicacao(n1=1, n2=1) {
+//     let resultado = 0;
+//     resultado = n1 * n2;
+//     return resultado;
+// }
+
+// console.log(multiplicacao(5));
+
+
+
+// //Divisão
+
+// function divisao(a1, a2) {
+//     let resDivisao = a1 / a2
+//     return resDivisao;
+// }
+
+// console.log(divisao(40, 2))
+
+// // Módulo
+
+// function modulo(z1, z2) {
+//     let resultado = z1 % z2
+//     return resultado;
+// }
+
+// console.log(modulo(5, 2));
