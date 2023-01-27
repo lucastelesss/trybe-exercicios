0653d916e2c12aa79433574c3255d293b2642554
lucastelesss <lucastelessouza005@gmail.com>
2023-01-27 19:01:42 -0300

    exercicios 1 e 2

--- fundamentos/secao-3-javascript-introducao/dia-05-javascript-es6/exercicios.js
+++ fundamentos/secao-3-javascript-introducao/dia-05-javascript-es6/exercicios.js
@@ -12,13 +12,27 @@
 //Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
 
   // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
-  const pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
-  pessoa = {
-    nome: 'Luna',
-    idade: 19
-  } // Altere essa estrutura para corrigir o erro.
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+//   const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+// //   pessoa = {
+// //     nome: 'Luna',
+// //     idade: 19
+// //   }
+
+// pessoa.nome = 'Luna',
+// pessoa.idade = '19'
+  
+// //   // Altere essa estrutura para corrigir o erro.
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+
+// Modifique a variável para que não ocorra Erro;
+
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hamburguer';
+// console.log(favoriteFood);
+
+//
