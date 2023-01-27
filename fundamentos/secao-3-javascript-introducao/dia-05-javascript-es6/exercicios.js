5ffc2f51e9815c0b1b611985d0eaf79a8ec96ae3
lucastelesss <lucastelessouza005@gmail.com>
2023-01-27 19:24:05 -0300

    exercicio 7

--- fundamentos/secao-3-javascript-introducao/dia-05-javascript-es6/exercicios.js
+++ fundamentos/secao-3-javascript-introducao/dia-05-javascript-es6/exercicios.js
@@ -75,3 +75,18 @@ console.log(numeroAleatorio());
   let nome = 'Ivan';
   console.log(hello(nome));
 
+
+
+// 🚀 Transforme a função nomeCompleto em uma arrow function;
+
+// function nomeCompleto(nome, sobrenome) {
+//     return `${nome} ${sobrenome}`
+//   }
+//   let nome = 'Ivan';
+//   let sobrenome = 'Pires';
+//   console.log(nomeCompleto(nome, sobrenome));
+
+const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+let nome = 'Ivan';
+let sobrenome = 'Pires'
+console.log(nomeCompleto(nome, sobrenome));
